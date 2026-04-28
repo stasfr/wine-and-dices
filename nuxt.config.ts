04ab78a6d8338d94d4e29bd0c22086867aa2f736
stas_fr <sfworking@yandex.ru>
2026-04-28 17:20:00 +0300

    setup modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    '@formkit/auto-animate',
+    '@pinia/colada-nuxt',
+  ],
+  css: ['~/assets/css/main.css'],
+});
