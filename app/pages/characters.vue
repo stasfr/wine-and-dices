bb7e66ebb62e7e0930a2b997a8e807de80aa7167
stas_fr <sfworking@yandex.ru>
2026-04-29 17:13:06 +0300

    Update characters.vue

--- app/pages/characters.vue
+++ app/pages/characters.vue
@@ -1,9 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { data } = useQuery({
+  key: ['characters'],
+  query: () => $fetch('/api/dices/characters/list'),
+});
+</script>
 
 <template>
   <UPage>
     <UPageHeader title="Dice Throne Characters List" />
 
-    <UPageBody> test </UPageBody>
+    <UPageBody>
+      <UPageList>
+        <UPageCard
+          v-for="(character, index) in data"
+          :key="index"
+          variant="ghost"
+          :to="character.to"
+          :target="character.target"
+        >
+          <template #body>
+            <UUser
+              :name="user.name"
+              :description="user.description"
+              :avatar="user.avatar"
+              size="xl"
+              class="relative"
+            />
+          </template>
+        </UPageCard>
+      </UPageList>
+    </UPageBody>
   </UPage>
 </template>
