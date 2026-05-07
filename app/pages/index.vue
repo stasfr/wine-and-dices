ed17cb5875dab7d41a895e21488ef540099ecbf6
stas_fr <sfworking@yandex.ru>
2026-05-07 18:53:13 +0300

    update main page for tests

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const { loggedIn, user, session, fetch, clear } = useUserSession();
+</script>
+
 <template>
-  <UPageHero title="Page Hero component" />
+  <div>
+    <pre>{{ loggedIn }}</pre>
+    <pre>{{ user }}</pre>
+    <pre>{{ session }}</pre>
+    <div class="flex gap-2">
+      <UButton @click="fetch">Fetch</UButton>
+      <UButton @click="clear">Clear</UButton>
+    </div>
+  </div>
 </template>
