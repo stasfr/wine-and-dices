364cef77bd827a49730d59bc155b7caa63fe3df3
stas_fr <sfworking@yandex.ru>
2026-05-08 00:05:09 +0300

    add btn and empty dialog

--- app/pages/games.vue
+++ app/pages/games.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const loading = ref(false);
+</script>
 
 <template>
   <UPage>
-    <UPageHeader title="Dice Throne Games List" />
+    <UPageHeader title="Dice Throne Games List">
+      <template #links>
+        <UModal :dismissible="loading === false" title="Create Game">
+          <UButton
+            label="Create Game"
+            color="success"
+            trailing-icon="i-lucide-square-plus"
+          />
 
-    <UPageBody> test </UPageBody>
+          <template #content>
+            <Placeholder class="h-48 m-4" />
+          </template>
+        </UModal>
+      </template>
+    </UPageHeader>
+
+    <UPageBody> here will be datatable </UPageBody>
   </UPage>
 </template>
