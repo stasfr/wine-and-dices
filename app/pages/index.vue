22e9bdbfb34fe3973bb76ad8ea95f757a726a821
stas_fr <sfworking@yandex.ru>
2026-05-13 20:59:40 +0300

    Replace index page content with under construction state

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,12 @@
-<script setup lang="ts">
-const { loggedIn, user, session, fetch, clear } = useUserSession();
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <pre>{{ loggedIn }}</pre>
-    <pre>{{ user }}</pre>
-    <pre>{{ session }}</pre>
-    <div class="flex gap-2">
-      <UButton @click="fetch">Fetch</UButton>
-      <UButton @click="clear">Clear</UButton>
-    </div>
+  <div class="flex flex-col items-center justify-center min-h-[60vh] gap-6">
+    <UIcon name="i-lucide-construction" class="w-16 h-16 text-primary" />
+    <h1 class="text-4xl font-bold text-center">Work in progress...</h1>
+    <p class="text-lg text-neutral-500 text-center max-w-md">
+      This application is under construction. You can explore the currently
+      available features in the header.
+    </p>
   </div>
 </template>
